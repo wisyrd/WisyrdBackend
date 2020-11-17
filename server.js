6327ba6eae48c1d8014245a42fec5ed0759943c4
Magnus Appel <68828420+magnuss86@users.noreply.github.com>
2020-11-17 15:26:49 -0800

    deploying

--- server.js
+++ server.js
@@ -27,11 +27,17 @@ require("../WisyrdBackend/routes/api")(app);
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wisyrddb", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}
-);
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/murmuring-falls-88355',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+  
 
 mongoose.connection.on('connected', ()=>{
     console.log('Mongoose is connected!')
