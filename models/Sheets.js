d09259217ff5504b54872ea075eb17fef0b8d586
Magnus Appel <68828420+magnuss86@users.noreply.github.com>
2020-11-20 14:25:10 -0800

    updated registration route

---
+++ models/Sheets.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const { ObjectID, ObjectId } = require("mongojs");
+
+
+
+const Schema = mongoose.Schema;
+
+const sheetsSchema = new Schema([
+    {
+
+        userid: {
+            //mongo object id
+            type: ObjectId
+        },
+        name: {
+            //mongo object id
+            type: String
+        },
+        sheetData: {
+            //mongo object id
+            type: {}
+        }
+
+         
+    }
+]);
+
+
+
+// need a get routes for find one sheet and all sheets (user)
+
+// post route for creating new sheets
+
+
+const Sheets = mongoose.model("Sheets", sheetsSchema);
+
+module.exports = Sheets;
+
